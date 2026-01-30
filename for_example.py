65eb029ac4c2ea771c977094fdc26430170dc26b
am <angelica31.marchese@gmail.com>
2026-01-30 11:13:21 +0100

    Session 5: operators, if, while

---
+++ for_example.py
@@ -0,0 +1,11 @@
+# for example
+# lets print the numbers 1 to 10
+
+for i in range(-10, 10, 2): # initial, end, step
+    print(i)
+
+# print multiplication table
+for i in range(1, 11):
+    for j in range(1, 11):
+        print(f"{i} x {j} = {i * j}")
+    print()
---
+++ readme.md
@@ -0,0 +1,8 @@
+#session 5-6
+
+## Various examples:
+-operators
+-exceptions
+-if examples
+-while examples
+-for examples
